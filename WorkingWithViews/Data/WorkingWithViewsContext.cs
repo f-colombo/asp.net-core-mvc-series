778e769feaf4ee2fbe1d7ea3d3e4d672bb5cee8d
f-colombo <freddycolombo@gmail.com>
2020-07-24 15:28:16 -0400

    Razor Pages and Partial Views

---
+++ WorkingWithViews/Data/WorkingWithViewsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WorkingWithViews.Models;
+
+namespace WorkingWithViews.Data
+{
+    public class WorkingWithViewsContext : DbContext
+    {
+        public WorkingWithViewsContext (DbContextOptions<WorkingWithViewsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WorkingWithViews.Models.Book> Book { get; set; }
+    }
+}
